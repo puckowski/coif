6e51698dd26877e509c82e5631a655caef8958bd
Daniel Puckowski <puckowski.d@gmail.com>
2023-11-24 08:38:20 -0500

    Add optimizations for COIFv5
    * Add optimizations for COIFv5.

--- ExampleProject/src/CircleBuilderCallable.java
+++ ExampleProject/src/CircleBuilderCallable.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -26,34 +27,22 @@ public class CircleBuilderCallable implements Callable {
 		final int radiusSquaredHalf2 = radiusSquared / 7;
 
 		int[] hist = new int[256];
-		for (int i = 0; i < hist.length; ++i) {
-			hist[i] = 0;
-		}
-
 		int[] hist2 = new int[256];
-		for (int i = 0; i < hist.length; ++i) {
-			hist2[i] = 0;
-		}
-
 		int[] hist3 = new int[256];
-		for (int i = 0; i < hist.length; ++i) {
-			hist3[i] = 0;
-		}
+		Arrays.fill(hist, 0);
+		Arrays.fill(hist2, 0);
+		Arrays.fill(hist3, 0);
 
-		for (int x = circleX - radius - 3; x < circleX + radius + 3; x++) {
-			if (x < 0 || x >= width)
-				continue;
-
-			for (int y = circleY - radius - 3; y < circleY + radius + 3; y++) {
-				if (y < 0 || y >= height)
-					continue;
-
-				double dx = x - circleX;
-				double dy = y - circleY;
-				double distanceSquared = dx * dx + dy * dy;
+		int y, dx, dy, distanceSquared, val;
+		
+		for (int x = Math.max(0, circleX - radius - 3); x < Math.min(width, circleX + radius + 3); x++) {
+		    for (y = Math.max(0, circleY - radius - 3); y < Math.min(height, circleY + radius + 3); y++) {
+				dx = x - circleX;
+				dy = y - circleY;
+				distanceSquared = dx * dx + dy * dy;
 
 				if (distanceSquared <= radiusSquared) {
-					int val = image[x][y];
+					val = image[x][y];
 					hist[val]++;
 
 					if (distanceSquared <= (radiusSquaredHalf)) {
@@ -78,44 +67,49 @@ public class CircleBuilderCallable implements Callable {
 		final int width = image.length;
 		final int height = image[0].length;
 
+		int x, y;
+		CircleResult cr, crSec, crThird, crFourth;
+		HistResult hr, hrSec, hrThird, hrFth;
+		int[] hist, hist2, center, histSec, hist2Sec, centerSec, histThird, hist2Third, centerThird, histFourth, hist2Fourth, centerFourth;
+		
 		for (MoravecResult mr : results) {
-			int x = mr.getX();
-			int y = mr.getY();
+			x = mr.getX();
+			y = mr.getY();
 
 			if (x - 4 >= 0 && y - 4 >= 0 && x + 4 < width && y + 4 < height) {
-				CircleResult cr = circles3(image, mr.getX(), mr.getY() - 4, radius);
-				int[] hist = cr.getHist();
-				int[] hist2 = cr.getInnerHist();
-				int[] center = cr.getCenterHist();
+				cr = circles3(image, mr.getX(), mr.getY() - 4, radius);
+				hist = cr.getHist();
+				hist2 = cr.getInnerHist();
+				center = cr.getCenterHist();
 
-				HistResult hr = new HistResult(hist, hist2, mr.getX(), mr.getY() - 4, center);
+				hr = new HistResult(hist, hist2, mr.getX(), mr.getY() - 4, center);
 				hr.computeDistances(binMergeCount);
 				hr.computeDistinctiveness(2);
 
-				CircleResult crSec = circles3(image, mr.getX() - 4, mr.getY(), radius);
-				int[] histSec = crSec.getHist();
-				int[] hist2Sec = crSec.getInnerHist();
-				int[] centerSec = crSec.getCenterHist();
+				crSec = circles3(image, mr.getX() - 4, mr.getY(), radius);
+				histSec = crSec.getHist();
+				hist2Sec = crSec.getInnerHist();
+				centerSec = crSec.getCenterHist();
 
-				HistResult hrSec = new HistResult(histSec, hist2Sec, mr.getX() - 4, mr.getY(), centerSec);
+				hrSec = new HistResult(histSec, hist2Sec, mr.getX() - 4, mr.getY(), centerSec);
 				hrSec.computeDistances(binMergeCount);
 				hrSec.computeDistinctiveness(2);
 
-				CircleResult crThird = circles3(image, mr.getX() + 4, mr.getY(), radius);
-				int[] histThird = crThird.getHist();
-				int[] hist2Third = crThird.getInnerHist();
-				int[] centerThird = crThird.getCenterHist();
+				crThird = circles3(image, mr.getX() + 4, mr.getY(), radius);
+				histThird = crThird.getHist();
+				hist2Third = crThird.getInnerHist();
+				centerThird = crThird.getCenterHist();
 
-				HistResult hrThird = new HistResult(histThird, hist2Third, mr.getX() + 4, mr.getY(), centerThird);
+				hrThird = new HistResult(histThird, hist2Third, mr.getX() + 4, mr.getY(), centerThird);
 				hrThird.computeDistances(binMergeCount);
 				hrThird.computeDistinctiveness(2);
 
-				CircleResult crFourth = circles3(image, mr.getX(), mr.getY() + 4, radius);
-				int[] histFourth = crFourth.getHist();
-				int[] hist2Fourth = crFourth.getInnerHist();
-				int[] centerFourth = crFourth.getCenterHist();
+				crFourth = circles3(image, mr.getX(), mr.getY() + 4, radius);
+				histFourth = crFourth.getHist();
+				hist2Fourth = crFourth.getInnerHist();
+				centerFourth = crFourth.getCenterHist();
 
-				HistResult hrFth = new HistResult(histFourth, hist2Fourth, mr.getX(), mr.getY() + 4, centerFourth);
+				hrFth = new HistResult(histFourth, hist2Fourth, mr.getX(), mr.getY() + 4, centerFourth);
 				hrFth.computeDistances(binMergeCount);
 				hrFth.computeDistinctiveness(2);
 
